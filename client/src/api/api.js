a53e4c886f31cdaffceee38d408ef6611c1068fd
ummairahmd <144005908+ummairahmd@users.noreply.github.com>
2026-09-20 01:34:22 +0800

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://copying-virginia-listed-cup.trycloudflare.com"
+    baseURL: "https://sura-melati-backend.onrender.com"
 });
 
 export default API;
