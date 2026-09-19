a9ab6f220c280e28038d953f9b7c57bf35a880c0
ummairahmd <144005908+ummairahmd@users.noreply.github.com>
2026-09-20 01:13:45 +0800

    Refactor ComplaintHistory component structure

--- client/src/customer/ComplaintHistory.jsx
+++ client/src/customer/ComplaintHistory.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import API from "../api/API";
+import API from "../api/api";
 import Swal from "sweetalert2";
 import "../assets/css/complaint_history.css";
 
